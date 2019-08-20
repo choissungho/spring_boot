652542c7329ada711cef653b21199cc04593aeb2
choissungho <chlsh0681@gmail.com>
2019-08-20 15:21:17 +0900

    commit

--- src/main/java/com/example/spring_boot/controller/MemberController.java
+++ src/main/java/com/example/spring_boot/controller/MemberController.java
@@ -4,10 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MemberController{
+public class MemberController {
+    @RequestMapping(value = "/member/home")
+    public void homeAction() {
+
+    }
+
     @RequestMapping(value = "/member/list")
-    public void memberAction(){
+    public void listAction() {
+
+    }
+
+    @RequestMapping(value = "/member/login")
+    public void loginAction() {
+
+    }
+
+    @RequestMapping(value = "/member/logout")
+    public void logoutAction() {
 
     }
 }
-
